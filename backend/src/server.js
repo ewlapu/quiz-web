7c1620013069737e651c7664c226a79aa1eb7326
ewlapu <anhhaohp24@gmail.com>
2026-04-22 00:16:33 +0700

    fix routing vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/database');
 
 const PORT = process.env.PORT || 2409;
 
+console.log("ENV CHECK:", process.env.MONGODB_URI);
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
