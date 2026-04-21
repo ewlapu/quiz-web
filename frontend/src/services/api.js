acbb0b6c961d796d5f960c58957e89361e6afa11
ewlapu <anhhaohp24@gmail.com>
2026-04-22 00:28:06 +0700

    fix api base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { store } from '../app/store';
 import { logout } from '../slices/authSlice';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:2409/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
